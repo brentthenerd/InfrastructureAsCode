53929910b9ae558e1d32bd05d974f591b6a478de
brentthenerd <31780620+brentthenerd@users.noreply.github.com>
2017-09-08 15:08:59 -0600

    Update AutomateAutomation.ps1

--- AutomateAutomation.ps1
+++ AutomateAutomation.ps1
@@ -1,17 +1,16 @@
-#1. https://chocolatey.org/install
-   Set-ExecutionPolicy AllSigned; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#2. https://chocolatey.org/packages/vagrant
-   choco install vagrant -y
-#3. https://chocolatey.org/packages/virtualbox
-   choco install virtualbox
-#Install git
-   choco install git.install/GitAndUnixToolsOnPath /WindowsTerminal
-#4. https://app.vagrantup.com/ubuntu/boxes/xenial64
-   vagrant init ubuntu/xenial64
+#Install chocolatey https://chocolatey.org/install
+Set-ExecutionPolicy AllSigned; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+#Install Vagrant https://chocolatey.org/packages/vagrant
+choco install vagrant -y
+
+#Install virtualbox https://chocolatey.org/packages/virtualbox
+choco install virtualbox
+
+#Install git
+choco install git.install/GitAndUnixToolsOnPath /WindowsTerminal
+
+#Install Ansible VM
 git clone https://github.com/davidobrien1985/vagrant_ansible.git
 cd vagrant_ansible
 Vagrant up
-
-
-https://app.vagrantup.com/centos/boxes/7
